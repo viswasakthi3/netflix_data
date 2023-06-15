5f80cb169d0d3c05647ae9d21cc2ce738f6fd0fa
vishwa <viswasakthi2@gmail.com>
2023-06-15 15:34:53 +0530

    .

--- popup.js
+++ popup.js
@@ -1,7 +1,17 @@
-document.getElementById('extractData').addEventListener('click', function() {
-    chrome.runtime.sendMessage({action: 'extractData'});
-});
-chrome.storage.local.get("netflixData", function(result) {
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: "fetchData" }, function (response) {
+      if (response.success) {
+        console.log(response.data);
+        chrome.storage.local.set({ netflixData: response.data }, () => {
+          console.log("Data saved in local storage");
+        });
+      } else {
+        console.log("Error fetching data");
+      }
+    });
+  });
+  
+  chrome.storage.local.get("netflixData", function (result) {
     console.log("Data retrieved from local storage:", result.netflixData);
   });
   

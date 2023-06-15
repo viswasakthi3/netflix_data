3e902f8d56926f10d435ca71a67b70379f2a561f
vishwa <viswasakthi2@gmail.com>
2023-06-15 15:55:46 +0530

    ,

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
 chrome.storage.local.get("netflixData", function(result) {
   console.log("Data retrieved from local storage:", result.netflixData);
 });
+document.getElementById("exportButton").addEventListener("click", function() {
+  // Your code to execute when the button is clicked
+});

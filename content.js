48d4411611dc7e13c7b3c88437f9139c02838213
vishwa <viswasakthi2@gmail.com>
2023-06-15 16:56:26 +0530

    m

--- content.js
+++ content.js
@@ -1,36 +1,30 @@
+function fetchAndParseData() {
+    // This function will be stringified and injected into the content page.
+    // It cannot reference any variables outside its scope.
+    const rows = document.querySelectorAll(".retableRow");
+    const data = [];
+
+    rows.forEach(row => {
+        const dateElement = row.querySelector(".col.date");
+        const titleElement = row.querySelector(".col.title a");
+
+        if (dateElement && titleElement) {
+            const date = dateElement.textContent.trim();
+            const title = titleElement.textContent.trim();
+
+            data.push({ date, title });
+        }
+    });
+
+    chrome.storage.local.set({netflixData: data}, function() {
+        console.log('Data saved in local storage');
+    });
+}
+
+// Listen for a message from the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'fetchData') {
-      fetch('https://www.netflix.com/viewingactivity')
-          .then(response => response.text())
-          .then(htmlString => {
-              const parser = new DOMParser();
-              const htmlDocument = parser.parseFromString(htmlString, "text/html");
-              const rows = htmlDocument.querySelectorAll(".retableRow");
-              const data = [];
-
-              rows.forEach(row => {
-                  const dateElement = row.querySelector(".col.date");
-                  const titleElement = row.querySelector(".col.title a");
-
-                  if (dateElement && titleElement) {
-                      const date = dateElement.textContent.trim();
-                      const title = titleElement.textContent.trim();
-
-                      data.push({ date, title });
-                  }
-              });
-
-              chrome.storage.local.set({netflixData: data}, function() {
-                  console.log('Data saved in local storage');
-              });
-
-              sendResponse({success: true});
-          })
-          .catch(error => {
-              console.error('Failed to fetch data', error);
-              sendResponse({success: false});
-          });
-
-      return true;  // Will respond asynchronously.
-  }
+    if (request.type === 'fetchData') {
+        fetchAndParseData();
+        sendResponse({success: true});
+    }
 });
--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-document.getElementById('button').addEventListener('click', () => {
-  chrome.runtime.sendMessage({type: 'fetchData'}, response => {
-      if (response.success) {
-          console.log('Data fetched successfully');
-      } else {
-          console.log('Failed to fetch data');
-      }
-  });
+document.getElementById('fetchButton').addEventListener('click', () => {
+    chrome.runtime.sendMessage({type: 'fetchData'}, response => {
+        if (response.success) {
+            console.log('Data fetched successfully');
+        } else {
+            console.error('Failed to fetch data');
+        }
+    });
 });
